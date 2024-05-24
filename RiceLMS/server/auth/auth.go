136a1215cbb3c923e487bb19cbfd0b4720183a02
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2024-05-24 00:04:25 -0500

    initialize auth package

--- RiceLMS/server/auth/auth.go
+++ RiceLMS/server/auth/auth.go
@@ -1,1 +1,37 @@
 package auth
+
+import (
+	"log"
+	"os"
+
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+var jwtKey []byte
+
+type Credentials struct {
+	Username string `json: "username"`
+	Password string `json: "password`
+}
+
+type Claims struct {
+	Username string `json: "username"`
+	jwt.RegisteredClaims
+}
+
+func Init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+	jwtKey = []byte(os.Getenv("JWT_SECRET"))
+	if len(jwtKey) == 0 {
+		log.Fatal("JWT_SECRET environment variable not set")
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- RiceLMS/server/main.go
+++ RiceLMS/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"server/auth"
 	"server/repository"
 )
 
 func main() {
 	repository.TestConnection()
+	auth.Init()
 }
