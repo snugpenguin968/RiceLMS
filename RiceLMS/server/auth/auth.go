a95b9d61ca4d7941ba1c11900a437a9694fb4d60
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2024-05-25 00:02:48 -0500

    token refresh functionality

--- RiceLMS/server/auth/auth.go
+++ RiceLMS/server/auth/auth.go
@@ -24,7 +24,7 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
-func Init() {
+func InitializeDB() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file:", err)
 	}
@@ -37,17 +37,17 @@ func Init() {
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	var creds Credentials
 	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	isAuthorized, err := repository.CheckCredentials(creds.Username, creds.Password)
 	if !isAuthorized {
-		w.WriteHeader(http.StatusUnauthorized)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -62,7 +62,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, *claims)
 	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -70,4 +70,50 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 
+}
+
+func RefreshHandler(w http.ResponseWriter, r *http.Request) {
+	var body map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tokenStr := body["token"]
+
+	claims := &Claims{}
+	//claims is a subset of token
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(tkn *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if !token.Valid {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	// Check token expiration and create a new token
+	if time.Until(claims.ExpiresAt.Time) > 30*time.Second {
+		http.Error(w, "Token is not yet expired", http.StatusBadRequest)
+		return
+	}
+
+	expirationTime := time.Now().Add(15 * time.Minute)
+	claims.ExpiresAt = jwt.NewNumericDate(expirationTime)
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	newTokenStr, err := newToken.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]string{"token": newTokenStr}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
--- RiceLMS/server/main.go
+++ RiceLMS/server/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"server/auth"
 	"server/repository"
 )
 
 func main() {
 	repository.InitializeConnection()
+	auth.InitializeDB()
+	http.HandleFunc("/login", auth.LoginHandler)
+	http.HandleFunc("/refresh", auth.RefreshHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
